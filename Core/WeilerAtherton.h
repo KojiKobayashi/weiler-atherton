0dc2ce7a7710bdd5c408df9f6be8ab8fd09bd738
KojiKobayashi <k.kobayashi@33i.co.jp>
2019-07-12 12:10:17 +0900

    refactor: hide nouse functions

--- Core/WeilerAtherton.h
+++ Core/WeilerAtherton.h
@@ -5,9 +5,10 @@
 namespace  WeilerAthertonAlgorithm {
 	class WeilerAtherton {
 	public:
+        std::list<Polygon> process(Polygon& subject, Polygon& clipping) const;
+	private:
 		static Point doWalk(std::list<Point>& enteringPoints, std::list<Point>& exitingPoints, std::list<Point>& walking_points, Polygon& pol, Point& start);
 		double distance(Point p, Point q) const;
 		void addPointInPlace(std::list<Point>& list, const Line& line, const IntersectionPoint& ip) const;
-		std::list<Polygon> process(Polygon& subject, Polygon& clipping) const;
 	};
 }
